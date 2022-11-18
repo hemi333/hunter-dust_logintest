17b78379d1b2dbaa8fc1367cd0e4be50821e2a2b
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-11-18 15:17:18 +0900

    cors 오류 수정
    cors 오류 수정

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
+const { sequelize } = require("./models/index");
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((error) => {
+    console.log(`데이터베이스 연결 실패 ${error}`);
+  });
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const cors = require("cors");
 const corsOption = {
   origin: true,
   credentials: true,
